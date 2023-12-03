48c01fb231422c1db3391d1dbcdeab0604d1940c
Hominsec <robinsond993@gmail.com>
2023-12-03 10:17:19 -0500

    LAB 5 COMPLETED

--- world.php
+++ world.php
@@ -4,9 +4,18 @@ error_reporting(E_ALL | E_STRICT);
 ?>
 
 <?php
-if (isset($_GET['query'])) {
+$temp=False;
+if (isset($_GET['query'])&&isset($_GET['lookup'])) {
+  $country = trim($_GET['query']);
+  $city = $_GET['lookup'];
+  $temp=True;
+  $bool=True;
+  //echo($country);
+}
+else if (isset($_GET['query'])) {
   $country = $_GET['query'];
   $bool=True;
+  //echo($country);
 } else {
   $bool=False;
 }
@@ -19,41 +28,80 @@ $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-if($bool==True){
+if($temp==True){
+  $stmt = $conn->prepare("SELECT cities.*
+    FROM cities
+    JOIN countries ON cities.country_code = countries.code
+    WHERE countries.name = :country");
+
+// Bind the parameter
+$stmt->bindParam(':country', $country, PDO::PARAM_STR);
+
+// Execute the statement
+$stmt->execute();
+
+// Fetch the results
+$results2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  /*
+  $stmt = $conn->query("SELECT cities.*
+  FROM cities
+  JOIN countries ON cities.country_code = countries.code
+  WHERE countries.name = '%$country%'");
+  $results2= $stmt->fetchAll(PDO::FETCH_ASSOC);*/
+}
+else if($bool==True){
   $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 else{
   $stmt = $conn->query("SELECT * FROM countries");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
-
-
-
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 
-
-<table>
- <thead>
- <tr>
- <th>Name</th>
- <th>Continent</th>
- <th>Independence</th>
- <th>Head of STate</th>
- </tr>
- </thead>
- <tbody>
- <?php foreach ($results as $row): ?>
-<tr>
- <td><?= $row['name']; ?></td>
- <td><?= $row['continent']; ?></td>
- <td><?= $row['independence_year']; ?></td>
- <td><?= $row['head_of_state']; ?></td>
-</tr>
- <?php endforeach; ?>
- </tbody>
-</table>
+<?php if ($temp): ?>
+    <table>
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>District</th>
+            <th>Population</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($results2 as $row): ?>
+            <tr>
+                <td><?= $row['name']; ?></td>
+                <td><?= $row['district']; ?></td>
+                <td><?= $row['population']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php elseif ($bool): ?>
+    <table>
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Continent</th>
+            <th>Independence</th>
+            <th>Head of State</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($results as $row): ?>
+            <tr>
+                <td><?= $row['name']; ?></td>
+                <td><?= $row['continent']; ?></td>
+                <td><?= $row['independence_year']; ?></td>
+                <td><?= $row['head_of_state']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php endif; ?>
 
 
